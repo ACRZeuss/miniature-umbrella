02600d5ebb32d707678b47b0251ba0c35be83af1
ACRZeuss <69459810+ACRZeuss@users.noreply.github.com>
2022-05-20 22:24:08 +0300

    update

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/url', slowDown({
       slug,
       url,
     });
-    if (url.includes('shortiny.tk')) {
+    if (url.includes('shortiny.herokuapp.com')) {
       throw new Error('Stop it. 🛑');
     }
     if (!slug) {
