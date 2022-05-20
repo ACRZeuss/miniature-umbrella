2c8bd466633622378ef3c0c8fba3ea2bd7b94ae4
ACRZeuss <69459810+ACRZeuss@users.noreply.github.com>
2022-05-20 22:34:19 +0300

    update

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/url', slowDown({
       slug,
       url,
     });
-    if (url.includes('shortiny.vercel.app')) {
+    if (url.includes('stny.ml')) {
       throw new Error('Stop it. 🛑');
     }
     if (!slug) {
