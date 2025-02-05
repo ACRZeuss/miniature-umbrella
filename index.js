f40417bd2955a2e9cdfd7765932a41d254dc1d31
Erhan Polat <ep4963807@gmail.com>
2025-02-05 22:54:09 +0300

    update

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/url', slowDown({
       slug,
       url,
     });
-    if (url.includes('shortiny.erhanpolat.net')) {
+    if (url.includes('erhanpolat.net')) {
       throw new Error('Stop it. 🛑');
     }
     if (!slug) {
